74644ec9f857171b9f1a3c093576920b3ee7431f
shilerhobbs <shilerhobbs@gmail.com>
2022-03-17 21:37:07 -0700

    added downloading files

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory, current_app
 from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
 
-upload_folder = './logs/'
+log_folder_name = 'logs'
+with app.app_context():
+    upload_folder = os.path.join(current_app.root_path, log_folder_name)
+
+if not os.path.exists(upload_folder):
+    os.makedirs(upload_folder)
 
 app.config['UPLOAD_FOLDER'] = upload_folder
 app.config['MAX_CONTENT_PATH'] = 500000
 
-incomes = [
-    {'description': 'salary', 'amount': 5000}
-]
 
-@app.route("/")
-def hello_world():
-    return "Hello world!!"
-
-@app.route('/incomes')
-def get_incomes():
-    return jsonify(incomes)
-
-
-@app.route('/incomes', methods=['POST'])
-def add_income():
-    incomes.append(request.get_json())
-    return '', 204
-
-@app.route('/upload', methods = ['GET', 'POST'])
-def upload_file():
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
     if request.method == 'POST':
         print(request.files)
         f = request.files['']
-        f.save(secure_filename(f.filename))
+        f.save(os.path.join(upload_folder, secure_filename(f.filename)))
         return 'file uploaded successfully'
+    if request.method == 'GET':
+        return 'nothing here yet'
+
+
+@app.route('/upload/logs/<path:filename>', methods=['GET'])
+def download(filename):
+    if filename in os.listdir('./logs'):
+        with app.app_context():
+            full_path = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
+        return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+    else:
+        return 'file not found'
+
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
